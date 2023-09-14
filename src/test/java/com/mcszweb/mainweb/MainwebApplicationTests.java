ba4cea77df646543fc15256ce9f232ed8e7015d8
Max Chan <max.chan@fdmgroup.com>
2023-09-14 09:53:57 +0800

    first commit

---
+++ src/test/java/com/mcszweb/mainweb/MainwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcszweb.mainweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
